9ebec289fef02dde0843c7754f880fd4e5e025b3
Quyen2010 <qn018231@gmail.com>
2024-08-06 16:30:37 +0700

    update

---
+++ Fashion_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fashion_Website.Startup))]
+namespace Fashion_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
